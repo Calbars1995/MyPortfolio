7c8b1c2cd8f9a04350e76e22b1cd32685ed9d724
Calbars1995 <cschleicher611@gmail.com>
2025-08-17 20:30:53 -0400

    about me button now disappears too

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function delay(ms) {
 //delayed display of the lines in about me section
 async function delayedDisplay()
 {
+    document.getElementById("aboutMeButton").style.display = "none";
     document.getElementById("aboutMePleaseClick").style.display = "none";
     //loop for showing the 6 about me lines
     for (let i = 1; i < 7; i++)
