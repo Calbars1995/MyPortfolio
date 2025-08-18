e8f4565e79c4badc6c8c8b13276f1fbf557fa1cb
Calbars1995 <cschleicher611@gmail.com>
2025-08-17 20:14:34 -0400

    About Me now shows a new line every second

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ function closeProject1()
 {
     document.getElementById("project1-content").style.display = "none";
 }
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 async function delayedDisplay()
 {
-    for (let i = 0; i < 7; i++)
+    for (let i = 1; i < 7; i++)
     {
-        document.getElementById("aboutMe" = i).style.display = "block";
+        document.getElementById("aboutMe" + i).style.display = "block";
+        document.getElementById("aboutMeLoading").style.display = "block";
         await delay(1000);
-        i++;
     }
-    
 }
