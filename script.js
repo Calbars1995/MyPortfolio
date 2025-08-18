dc42e0365a09f488d826c46f812ed30079a4c0cb
Calbars1995 <cschleicher611@gmail.com>
2025-08-17 20:19:04 -0400

    about me delayed showing is now working as intended, unless i find an issue later....

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ function closeProject1()
 {
     document.getElementById("project1-content").style.display = "none";
 }
+//delay function that can be used later to delay things
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+//delayed display of the lines in about me section
 async function delayedDisplay()
 {
+    //loop for showing the 6 about me lines
     for (let i = 1; i < 7; i++)
     {
         document.getElementById("aboutMe" + i).style.display = "block";
         document.getElementById("aboutMeLoading").style.display = "block";
         await delay(1000);
     }
+    document.getElementById("aboutMeLoading").style.display = "none";
+    document.getElementById("aboutMeLoaded").style.display = "block";
 }
